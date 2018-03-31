34bfb14000ccd66d8fdb4c96fdb7ebe0bc9c7906
Olivia Wilson <olivia@rtwilson.com>
2018-03-31 20:34:22 +0100

    Made script into a function calcAntipodes and called it

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var centroid = turf.point([-1.410151, 50.927559]);
-var from_loc = turf.point([ -1.442767, 50.930831]);
+
+function calcAntipodes (centroid, from_loc) {
+
 var options = {units: 'miles'};
 
 var distance_to_centroid = turf.distance(from_loc, centroid, options);
@@ -8,3 +9,11 @@ var bearing_to_centroid = turf.bearing(from_loc, centroid);
 var destination = turf.rhumbDestination(centroid, distance_to_centroid, bearing_to_centroid, options);
 console.log(distance_to_centroid);
 console.log(destination);
+
+return destination
+
+}
+
+var centroid = turf.point([-1.410151, 50.927559]);
+var from_loc = turf.point([ -1.442767, 50.930831]);
+var destination = calcAntipodes(centroid, from_loc);
